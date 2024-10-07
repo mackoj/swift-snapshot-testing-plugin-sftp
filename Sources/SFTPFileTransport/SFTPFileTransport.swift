930cb4f79dafb9a1078e3cdbfb0f6f4c8cf114c0
Jeffrey Macko <github.jm@macko.fr>
2024-10-07 02:02:16 +0200

    feat: ugly and bad code

--- Sources/SFTPFileTransport/SFTPFileTransport.swift
+++ Sources/SFTPFileTransport/SFTPFileTransport.swift
@@ -9,21 +9,21 @@ import FileSerializationPlugin
  */
 
 public actor SFTPFileTransport: @preconcurrency FileSerializationPlugin {
+  public typealias Configuration = SSHClientConnectConfiguration
+  
+  public struct SSHClientConnectConfiguration {
+    let host: String
+    let port: Int = 22
+    let authenticationMethod: SSHAuthenticationMethod
+    let hostKeyValidator: SSHHostKeyValidator
+    let reconnect: SSHReconnectMode
+    let algorithms: SSHAlgorithms = SSHAlgorithms()
+  }
 
   var sftp: SFTPClient? = nil
   var client: SSHClient? = nil
 
   public init() {}
-
-  func startClient() async throws {
-    client = try await SSHClient.connect(
-      host: "example.com",
-      authenticationMethod: .passwordBased(username: "joannis", password: "s3cr3t"),
-      hostKeyValidator: .acceptAnything(), // Please use another validator if at all possible, it's insecure
-      reconnect: .never
-    )
-    sftp = try await client?.openSFTP()
-  }
   
   public static var location = FileSerializationLocation.plugins("sftp")
   
@@ -40,8 +40,20 @@ public actor SFTPFileTransport: @preconcurrency FileSerializationPlugin {
     return data
   }
   
-  deinit {
-//    try await client?.close()
+  public func start(_ configuration: Configuration) async throws {
+    client = try await SSHClient.connect(
+      host: configuration.host,
+      port: configuration.port,
+      authenticationMethod: configuration.authenticationMethod,
+      hostKeyValidator: configuration.hostKeyValidator,
+      reconnect: configuration.reconnect,
+      algorithms: configuration.algorithms
+    )
+    sftp = try await client?.openSFTP()
+  }
+  
+  public func stop() async throws {
+    try await client?.close()
   }
   
   public static let identifier: String = "FileSerializationPlugin.sftp"
